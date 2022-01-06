6e90ec506cc3e364d9993ff6c9fa76261486c841
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-06 17:59:24 -0300

    atualizado

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const { response } = require('express');
+const { listen } = require('express/lib/application');
 
 require('./database')
 
@@ -8,4 +10,3 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
-app.listen(5000)
