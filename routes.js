ccdbee2680c72943dce02d8e67fe07f5df3e79ea
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-07 08:56:55 -0300

    teste

--- routes.js
+++ routes.js
@@ -4,19 +4,5 @@ const ProdutoController = require('./controllers/ProdutoController');
 
 const routes = express.Router()
 
-routes.post('/cadastro_produtos', ProdutoController.criar
-)
-routes.get('/lista_produtos', function (req, res) {
-  res.send("OI")
-}
-)
-routes.get('/lista_produtos/:id', ProdutoController.listar_id
-)
-routes.put('/update/:id', ProdutoController.update
-)
-routes.delete('/delete/', ProdutoController.delete
-)
-
-
 
 module.exports = routes
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
+app.get('/', function (req, res) {
+  res.send("OLA")
+})
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
