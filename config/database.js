51774549a255fb16f4549f733640d30ce22cb3ca
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-07 16:36:19 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'us-cdbr-east-05.cleardb.net',
   username: 'b1ed4bbc8dfaf2',
   password: 'a70e8c34',
-  database: 'compras',
+  database: 'heroku_c146f151144b7dc',
   define: {
     timestamps: true,
     underscored: true,
