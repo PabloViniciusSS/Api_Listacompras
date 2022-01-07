107d245b2096f6b4209aa4247621bbed3668d0a8
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-07 08:46:36 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,17 +4,10 @@ const ProdutoController = require('./controllers/ProdutoController');
 
 const routes = express.Router()
 
-routes.post('/cadastro_produtos', ProdutoController.criar
-)
-routes.get('/', function (req, res) {
+
+app.get('/', function (req, res) {
   res.render('Ola')
 }
-)
-routes.get('/lista_produtos/:id', ProdutoController.listar_id
-)
-routes.put('/update/:id', ProdutoController.update
-)
-routes.delete('/delete/', ProdutoController.delete
 )
 
 
