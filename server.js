6fce03730c1ad56c9345ce1cfc2004625746cc08
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-07 07:43:40 -0300

    atualizacao

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-var http = require('http');
 const express = require('express');
 const routes = require('./routes');
 const { response } = require('express');
 const { listen } = require('express/lib/application');
-http.createServer(function(req, res) {
-  response.writeHead(200, {"Content-type": "text/plain"});
-  response.end("Entro\n")
-}); listen(process.env.PORT)
 
 require('./database')
 
@@ -15,3 +10,4 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
+const port = process.env.PORT || 3000;
