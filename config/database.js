43a15e539e13902f1086da49fa13304377ad92e0
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-01-07 16:34:58 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'heroku_c146f151144b7dc',
+  host: 'us-cdbr-east-05.cleardb.net',
   username: 'b1ed4bbc8dfaf2',
   password: 'a70e8c34',
   database: 'compras',
